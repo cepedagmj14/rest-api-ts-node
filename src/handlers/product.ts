1571b8df1bdc1193b8bfee52921911942bac86da
mcepeda <mcepeda@xcala.com>
2024-08-31 19:54:21 -0400

    validations in handler or router

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,7 +1,29 @@
 import { Request, Response } from "express";
 import Product from "../models/Product.model";
+import { check, validationResult } from "express-validator";
 
 export const createProduct = async (req: Request, res: Response) => {
+  // validacion en el handler
+  //   await check("name")
+  //     .notEmpty()
+  //     .withMessage("El nombre del producto no puede ir vacio")
+  //     .run(req);
+
+  //   await check("price")
+  //     .isNumeric()
+  //     .withMessage("Valor no valido")
+  //     .custom((value) => value > 0)
+  //     .withMessage("Precio no valido")
+  //     .notEmpty()
+  //     .withMessage("El precio del producto no puede ir vacio")
+  //     .run(req);
+
+  let errors = validationResult(req);
+  console.log("errors check", errors);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   const savedProduct = await Product.create(req.body);
 
   res.json({ data: savedProduct });
--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,29 @@
 import { Router } from "express";
 import { createProduct } from "./handlers/product";
-
+import { body } from "express-validator";
 const router = Router();
 // routing
 router.get("/", (req, res) => {
   res.json("desde get");
 });
 
-router.post("/", createProduct);
+router.post(
+  "/",
+
+  // validacion en el router
+  body("name")
+    .notEmpty()
+    .withMessage("El nombre del producto no puede ir vacio"),
+
+  body("price")
+    .isNumeric()
+    .withMessage("Valor no valido")
+    .custom((value) => value > 0)
+    .withMessage("Precio no valido")
+    .notEmpty()
+    .withMessage("El precio del producto no puede ir vacio"),
+  createProduct
+);
 
 router.put("/", (req, res) => {
   res.json("desde put");
