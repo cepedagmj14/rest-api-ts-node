122dc1b2ccc08d4e3a63badae9ed47ec408e570c
mcepeda <mcepeda@xcala.com>
2024-10-20 20:51:14 -0300

    test get product by id

--- __test__/handlers/products.test.ts
+++ __test__/handlers/products.test.ts
@@ -68,3 +68,31 @@ describe('GET /api/products', () => {
     expect(response.status).not.toBe(400);
   });
 });
+
+describe('GET /api/products/:id', () => {
+  it('should return a 404 response for a not-exist product', async () => {
+    const productoID = 2000;
+    const response = await request(server).get(`/api/products/${productoID}`);
+
+    expect(response.status).toBe(404);
+    expect(response.body).toHaveProperty('error');
+    expect(response.body.error).toBe('producto no encontrado');
+  });
+
+  test('should a valid id the url', async () => {
+    const idNoValid = 'string';
+    const response = await request(server).get(`/api/products/${idNoValid}`);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('errors');
+    expect(response.body.errors[0].msg).toBe('ID no valido');
+  });
+
+  test('get a JSON response for a single product', async () => {
+    const productoID = 1;
+    const response = await request(server).get(`/api/products/${productoID}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('data');
+  });
+});
